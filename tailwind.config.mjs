08ef2aeaf2c938e357c28756a616fe237f4abc9e
Christophe anfry <frybcn@gmail.com>
2023-11-21 10:37:20 +0100

    Add Navbar + footer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(#27272a 0%, #18181b 60%)',
+      },
+    },
+  },
+  plugins: [],
+};
